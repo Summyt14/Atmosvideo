cdaae40a18a3fb3ea9b77c93b6458c0d544f807c
José Siopa <siopajose@outlook.com>
2023-05-24 19:01:23 +0100

    Possibility to plot the energy calculation

--- video_properties.py
+++ video_properties.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import threading
 import queue
+import matplotlib.pyplot as plt
 
 DISCONNECTED = 0
 RUNNING = 1
@@ -20,25 +21,27 @@ class ExtractVideoProperties:
         self.lock = threading.Lock()
         self.queue = queue.Queue()
 
-    def start_extracting(self, video_path: str, width: int):
+    def start_extracting(self, video_path: str, width: int, do_plot: bool = False):
         """
         Start extracting properties from the video.
 
         Args:
             video_path (str): The path of the video stream.
             width (int): The width which the video will be resized to.
+            do_plot: Show the movement flow image.
         """
         self.status = RUNNING
-        self.thread = threading.Thread(target=self.run_thread, args=(video_path, width))
+        self.thread = threading.Thread(target=self.run_thread, args=(video_path, width, do_plot))
         self.thread.start()
 
-    def run_thread(self, video_path: str, width: int):
+    def run_thread(self, video_path: str, width: int, do_plot: bool):
         """
         Runs the thread for capturing and processing frames from the video.
 
         Args:
             video_path (str): The path of the video stream.
             width (int): The width which the video will be resized to.
+            do_plot: Show the movement flow image.
         """
         cap = cv2.VideoCapture(video_path)
         cap_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -61,7 +64,7 @@ class ExtractVideoProperties:
                 print("Error reading the frames of the video")
                 self.status = ERROR
 
-            energy = self.calculate_energy(frame, prev_frame)
+            energy, plot = self.calculate_energy(frame, prev_frame, do_plot)
 
             # Convert frame to HSV color space
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -72,7 +75,7 @@ class ExtractVideoProperties:
             hue = hsv[:,:,0].mean()
             saturation = hsv[:,:,1].mean()
             brightness = hsv[:,:,2].mean()
-            self.queue.put((frame_num, energy, hue, saturation, brightness))
+            self.queue.put((frame_num, energy, hue, saturation, brightness, plot))
 
             prev_frame = frame
             frame_num += 1
@@ -86,13 +89,14 @@ class ExtractVideoProperties:
         cap.release()
         self.lock.release()
 
-    def calculate_energy(self, frame, prev_frame):
+    def calculate_energy(self, frame, prev_frame, do_plot):
         """
         Calculate the energy from the previous frame and the current frame.
 
         Args:
-            frame: The current frame
-            prev_frame: The previous frame
+            frame: The current frame.
+            prev_frame: The previous frame.
+            do_plot: Show the movement flow image.
         """
         # Convert frames to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -101,7 +105,19 @@ class ExtractVideoProperties:
         flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
         # Calculate the energy as the sum of squared magnitudes of the flow vectors
         energy = np.sum(flow[..., 0]**2 + flow[..., 1]**2)
-        return energy
+        plot = None
+
+        if do_plot:
+            magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+
+            # Create an RGB image to visualize the flow
+            hsv1 = np.zeros_like(prev_frame)
+            hsv1[..., 1] = 255
+            hsv1[..., 0] = angle * 180 / np.pi / 2
+            hsv1[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+            plot = cv2.cvtColor(hsv1, cv2.COLOR_HSV2BGR)
+
+        return energy, plot
     
     def shutdown(self):
         """
@@ -126,11 +142,14 @@ class ExtractVideoProperties:
 
 
 if __name__ == "__main__":
-    path = 'example_videos/v4.mp4'
+    path = 'example_videos/v1.mp4'
     video_extractor = ExtractVideoProperties()
     video_extractor.start_extracting(path, 320)
     while video_extractor.status == RUNNING:
         values = video_extractor.get_values()
         if values is not None:
-            frame_num, energy, hue, saturation, brightness = values
+            frame_num, energy, hue, saturation, brightness, plot = values
+            if plot is not None:
+                plt.imshow(plot)
+                plt.pause(0.001)
             print(f"Frame {frame_num}: Energy={energy}, HUE={hue}, Saturation={saturation}, Brightness={brightness}")
